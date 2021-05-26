440b249d3a205729a44f6ac098cdbb1cdf566d9b
potemkuh@gmail.com <potemkuh@gmail.com>
2021-05-26 19:43:24 +0300

    add --format

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
 gendiff:
 	poetry run gendiff
+
+install:
+	poetry install
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python -m pip install dist/*.whl
--- gendiff/arg_parse.py
+++ gendiff/arg_parse.py
@@ -1,16 +1,11 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate getdiff')
+    parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument(
         '-f ',
         '--format',
         help='set format of output')
-    args = parser.parse_args()
-    print(args.first_file, args.second_file)
-
-
-if __name__ == '__main__':
-    main()
+    return parser.parse_args()
