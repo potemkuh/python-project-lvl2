c579d16b6b24328ed7b429b6ce8d7226548cf3f9
grins <potemkuh@gmail.com>
2021-06-17 20:27:31 +0300

    test print

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -14,14 +14,16 @@ def calculate_diff(dict1, dict2):
             result.append({
                 'key': key,
                 'state': 'minus',
-                'value': dict1.get(key)
+                'value': dict1[key]
             })
+            continue
         elif key not in dict1:
             result.append({
                 'key': key,
                 'state': 'plus',
-                'value': dict2.get(key)
+                'value': dict2[key]
             })
+            continue
         elif isinstance(dict1[key], dict):
             if isinstance(dict2[key], dict):
                 result.append({
@@ -30,31 +32,14 @@ def calculate_diff(dict1, dict2):
                     'CHILDREN': calculate_diff(dict1[key], dict2[key]),
                 })
                 continue
-        elif key in dict1 and key in dict2:
-            if dict1.get(key) != dict2.get(key):
-                result.append({
-                'key': key,
-                'state': 'minus',
-                'value': dict1[key]
-            })
-                result.append({
-                'key': key,
-                'state': 'plus',
-                'value': dict2[key]
-            })
-            else:
-                result.append({
-                'key': key,
-                'state': 'pass',
-                'value': dict1[key]
-            })
-        elif dict1[key] != dict2[key]:
-            result.append({
+        
+        result.append({
             'key': key,
             'state': 'updated',
             'old_value': dict1[key],
             'new_value': dict2[key],
             })
+            
     return result
 
 
@@ -65,21 +50,19 @@ def generate_diff(data1, data2):
     
 
 def my_print(keys, space = ''):
-    result_str = '{\n'
+    result_str = ''
+    depth = '    '
     #print(keys, '\n')
     for key in keys:
-        print(key, '\n')
-        if key['state'] == 'plus':
+        if key['state'] == 'NESTED':
+            result_str += f'{space}   {key["key"]}: ' + '{\n'
+            res = my_print(key['CHILDREN'], space + depth)
+            result_str += res
+        elif key['state'] == 'updated':
+            result_str += f'{space} - {key["key"]}: {key["new_value"]}\n'
+            result_str += f'{space} - {key["key"]}: {key["old_value"]}\n'
+        elif key['state'] == 'plus':
             result_str += f'{space} + {key["key"]}: {key["value"]}\n'
         elif key['state'] == 'minus':
             result_str += f'{space} - {key["key"]}: {key["value"]}\n'
-        elif key['state'] == 'pass':
-            result_str += f'{space}   {key["key"]}: {key["value"]}\n'
-        elif key['state'] == 'NESTED':
-            result_str += f'   {key["key"]}: '
-            res = my_print(key['CHILDREN'], space = '     ')
-            result_str += res 
-        elif key['state'] == 'updated':
-            result_str += f'{space} {key["key"]}: {key["new_value"]}\n' #+
-            result_str += f'{space} {key["key"]}: {key["old_value"]}\n' #-
-    return result_str + '\n'
+    return result_str
