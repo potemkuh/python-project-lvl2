ec5fefe900c764da32e09532d8076e6a1ebcae0c
grins <potemkuh@gmail.com>
2021-07-18 13:54:15 +0300

    edit diff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,8 @@ def main():
     f1 = args().first_file
     f2 = args().second_file
     format = args().format
-    generate_diff(f1, f2, format)
+    diff = generate_diff(f1, f2, format)
+    print(diff)
 
 if __name__ == '__main__':
     main()
