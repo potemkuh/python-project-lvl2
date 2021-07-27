98fddd826c46850c15570194106f659c531b9aa7
grins <potemkuh@gmail.com>
2021-07-27 13:53:50 +0300

    fix test_flat_files

--- tests/test_flat_files.py
+++ tests/test_flat_files.py
@@ -1,7 +1,7 @@
 from gendiff.gendiff import generate_diff
 
 
-def test_gendiff_json():    
+def test_gendiff_json():
     data1 = 'gendiff/files/file1.json'
     data2 = 'gendiff/files/file2.json'
     assert generate_diff(data1, data2) == open('tests/fixtures/stylish_flat_files.txt', 'r').read()
