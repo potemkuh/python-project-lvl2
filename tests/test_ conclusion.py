ee750c0e450e6b45162911fa5e2e5089525b5485
grins <potemkuh@gmail.com>
2021-08-09 18:13:58 +0300

    refractoring 2.1

--- tests/test_ conclusion.py
+++ tests/test_ conclusion.py
@@ -7,18 +7,18 @@ test_cases = ['yml', 'json']
 
 @pytest.mark.parametrize('test_format', test_cases)
 def test_gendiff_stylish(test_format):
-    data1 = f'gendiff/files/file_test1.{test_cases}'
-    data2 = f'gendiff/files/file_test2.{test_cases}'
+    data1 = f'tests/fixtures/file_test1.{test_format}'
+    data2 = f'tests/fixtures/file_test2.{test_format}'
     assert generate_diff(data1, data2) == open('tests/fixtures/stylish_recursive.txt', 'r').read()
 
 @pytest.mark.parametrize('test_format', test_cases)
 def test_gendiff_plain(test_format):
-    data1 = f'gendiff/files/file_test1.{test_cases}'
-    data2 = f'gendiff/files/file_test2.{test_cases}'
+    data1 = f'tests/fixtures/file_test1.{test_format}'
+    data2 = f'tests/fixtures/file_test2.{test_format}'
     assert generate_diff(data1, data2, format='plain') == open('tests/fixtures/plain_flat.txt', 'r').read()
 
 @pytest.mark.parametrize('test_format', test_cases)
 def test_gendiff_json(test_format):
-    data1 = f'gendiff/files/file_test1.{test_cases}'
-    data2 = f'gendiff/files/file_test2.{test_cases}'
+    data1 = f'tests/fixtures/file_test1.{test_format}'
+    data2 = f'tests/fixtures/file_test2.{test_format}'
     assert generate_diff(data1, data2, format='json') == open('tests/fixtures/json.txt', 'r').read()
